014e2fccff0640ebdc3b52a59ac3e042ccde1056
Naitik Rishu <naitikrishu@Naitiks-MacBook-Air.local>
2026-04-03 21:56:06 +0530

    set up routes

--- server/index.js
+++ server/index.js
@@ -1,17 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDb from "./config/connectDb.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      const isLocalhostOrigin = /^http:\/\/(localhost|127\.0\.0\.1):\d+$/.test(origin);
+      if (isLocalhostOrigin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (_req, res) => {
   res.json({ ok: true, message: "InterVue server running" });
 });
 
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
 const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDb();
 });
